aa6dcb58317b33b69e0e961ba2cba5e7d30198ff
pontshoL <pontshol29@gmail.com>
2022-08-04 16:14:42 +0200

    first commit

--- src/Components/addHotel.js
+++ src/Components/addHotel.js
@@ -1,48 +1,121 @@
 import '../CSS/addHotel.css'
-import {db} from '../Config/firebase'
-import React,{useState} from 'react';
-import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../Config/firebase'
+import React, { useEffect, useState } from 'react';
+import { addDoc, collection, deleteDoc, Firestore, getDocs, doc } from 'firebase/firestore';
 import { useNavigate } from "react-router-dom";
+import { async } from '@firebase/util';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 
-function AddHotel(){
+function AddHotel() {
     const [hotelName, setHotelName] = useState("")
     const [hotelLocation, setHotelLocation] = useState("")
     const [hotelDescription, setHotelDescription] = useState("")
     const [hotelAmount, setHotelAmount] = useState("")
-    const addHotelRef = collection(db,'hotel')
+    const addHotelRef = collection(db, 'hotel')
     const navigate = useNavigate()
 
 
-    const addHotel = () =>{
-        const hotel ={
-            name: hotelName,
-            location: hotelLocation,
-            description: hotelDescription,
-            amount: hotelAmount,
-        }
+    const [img, setImg] = React.useState()
+    // const collectionRef = collection(Firestore, 'hotels')
 
-        //push to firestore
-        addDoc(addHotelRef,hotel).then(()=>{
-            console.log('added')
-            alert('added successfully')
-            navigate("/room-list")
-        }).catch((err)=>{
-            console.log(err)
-        })
+    const addHotel = async () => {
+        const storageRef = ref(Storage, `/IMAGES/${img.name}`)
+        await uploadBytesResumable(storageRef, img).then((uploadTask) => {
+            getDownloadURL(uploadTask.ref).then((url) => {
+                console.log(url);
+                // const addHotel = () =>{
+                const hotel = {
+                    name: hotelName,
+                    location: hotelLocation,
+                    description: hotelDescription,
+                    amount: hotelAmount,
+                    image: url
+                }
+
+                //push to firestore
+                addDoc(addHotelRef, hotel).then(() => {
+                    console.log('added')
+                    alert('added successfully')
+                    navigate("/room-list")
+                }).catch((err) => {
+                    console.log(err)
+                })
+                // }
+
+            })
+        }).then(() => {
+            alert("hotel added successfully")
+        }).catch((err)=>{console.log(err);})
     }
 
-    return(
+
+
+
+    const [hotels, setHotels] = useState([])
+
+    //getting hotels
+    const getData = async () => {
+        let data = await getDocs(collection(db, "hotel"))
+        setHotels(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    }
+
+
+    useEffect(() => {
+
+
+
+        getData()
+
+
+    }, [])
+    console.log("hotels", hotels);
+
+    //delete function
+    function deleteHotel(id) {
+        const getDoc = doc(db, 'hotel', id)
+        deleteDoc(getDoc).then(() => {
+            alert('deleted successfully')
+        }).catch(err => {
+            console.log(err)
+        })
+
+    }
+
+    const [form, setForm] = useState({image: ""});
+
+    const handleImage = (e) => {
+        setForm({...form,image:e.target.files[0]});
+    };
+
+    return (
         <div>
             <p className='heading'>HOTEL DETAILS</p>
 
-         <div  className='form'>
-           <input className='hotelDetails' type="text" placeholder="Hotel Name" onChange={(e)=>setHotelName(e.target.value)}/><br></br>
-           <input className='hotelDetails' type="text" placeholder="Location" onChange={(e)=>setHotelLocation(e.target.value)}/><br></br>
-           <input className='hotelDetails' type="text" placeholder="Description" onChange={(e)=>setHotelDescription(e.target.value)}/><br></br>
-           <input className='hotelDetails' type="number" placeholder="Amount" onChange={(e)=>setHotelAmount(e.target.value)}/><br></br>
-           <button className='btnAdd' onClick={(e)=>{addHotel()}}>ADD</button> 
+            <div className='form'>
+                <input className='hotelDetails' type="text" placeholder="Hotel Name" onChange={(e) => setHotelName(e.target.value)} /><br></br>
+                <input className='hotelDetails' type="text" placeholder="Location" onChange={(e) => setHotelLocation(e.target.value)} /><br></br>
+                <input className='hotelDetails' type="text" placeholder="Description" onChange={(e) => setHotelDescription(e.target.value)} /><br></br>
+                <input className='hotelDetails' type="number" placeholder="Amount" onChange={(e) => setHotelAmount(e.target.value)} /><br></br>
+                <input type="file" placeholder="Choose File" accept='image/*' onchange={handleImage}/>
+                <button className='btnAdd' onClick={(e) => { addHotel() }}>ADD</button>
+            </div>
+            {hotels.map((hotel) => (
+                <div className='output'>
+                    <div className='results'>
+                        <div className='picture'></div>
+                        <div className='Description'>
+                            <h2>{hotel.name}<br></br>{hotel.location}<br></br>{hotel.description}<br></br>{hotel.amount}</h2>
+                        </div>
+                        <div className='Buttons'>
+                            <button className='btn1'>Update</button><br></br>
+                            <button className='btn2' onClick={(e) => { deleteHotel(hotel.id) }}>Delete</button>
+                        </div>
 
-        </div>
+
+                    </div>
+                </div>
+            ))}
+
         </div>
     )
 }
--- src/Components/admin.js
+++ src/Components/admin.js
@@ -1,36 +1,74 @@
 import '../CSS/admin.css'
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../Config/firebase';
 
 
 function Admin(){
-    const navigate = useNavigate();
-    
-    const handleRoom = ()=>{
-        navigate("/add-room")
-       }
+  const [hotels,setHotels]=useState([])
+  useEffect(()=>{
+    const getData= async ()=>{
+     let data=await getDocs(collection(db,"hotel"))
+      setHotels(
+        data.docs.map((doc)=>(
+          doc.data()
+        )
+      )
+      )
+    }
+    getData()
+  },[])
+  console.log("hotels",hotels);
     return(
-        <div className='main'>
-           <Navbar bg="dark" variant="dark">
-           <Container className='container1'>
-           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+        <div className='cover'>
+            <div className='main'>
+              <Navbar bg="dark" variant="dark">
+                <Container className='container1'>
+                  <Nav className="me-auto">
+                    <div className='home'>
+                      <Nav.Link href="#home">Home</Nav.Link>
+                    </div>
+                    <Nav.Link href="#Bookings">Bookings</Nav.Link>
+                    <Nav.Link href="#Contact">Contact</Nav.Link>
+                    <Nav.Link href="add-room">Manage</Nav.Link>
+                  </Nav>
+                </Container>
+              </Navbar>
+            </div>
+             
+         
+            <div className='div-cards'>
+            {
+              hotels.map((hotel)=>(
+                
+                  <div className='cards'>
+                <img src='hotel4.jpg' alt='hotel1' className='img' />
+                <span>{hotel.name}</span><br></br>
+                <span>{hotel.location}</span><br></br>
+                <span>{hotel.description}</span>
+                <span>{hotel.amount}</span>
+              </div>
+                 
+              ))
+            }
+             
+
+             </div>
+            
+        
+            
+              
          
 
-           <div className='content'>
-            <button className='btn1' onClick={handleRoom}>ADD ROOM</button>
-           {/* <img src='logo.jfif' alt='pic' className='img'/> */}
-           </div>
+           
+         
+     
+           
         </div>
     )
 }
